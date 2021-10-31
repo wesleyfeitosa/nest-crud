b0d64ea6b56611c6c3284ec1defe644e0421b697
Wesley Feitosa <jwesleydasilva@gmail.com>
2021-10-31 16:34:45 -0300

    feat: finalizando crud de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.1q27l.mongodb.net/test',
+      {
+        useNewUrlParser: true,
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
